84f462d28f95c4cea1b060cfd3d0961765b0bc8c
Monish-06 <70092044+Monish-06@users.noreply.github.com>
2025-04-12 09:59:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,19 @@
+import threading
+import requests
+import time
+ 
+def keep_alive():
+    while True:
+        try:
+            requests.get("https://scornful-andreana-moxi35-c66f799a.koyeb.app/")
+        except:
+            pass
+        time.sleep(90)  # Ping every 5 minutes
+ 
+threading.Thread(target=keep_alive, daemon=True).start()
+
+
+
 import logging
 import logging.config
 # Credit @LazyDeveloper.
